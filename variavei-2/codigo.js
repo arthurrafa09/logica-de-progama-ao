be0c519c13019ccd959ce62c2b964234b314110c
Arthur <mioliara2020@gmail.com>
2025-06-03 09:07:47 -0300

    codigo.js

---
+++ variavei-2/codigo.js
@@ -0,0 +1,19 @@
+console.log (&quot;Boas vindas a loja, THstore, para concluir seus
+cadastro forneça as seguintes informaçoes&quot;)
+let nome = prompt(&quot;Digite seu nome: &quot;)
+let idade = Number(prompt(&quot;Digite seu idade: &quot;))
+let email = prompt(&quot;Digite seu email: &quot;)
+let telefone = Number(prompt(&quot;Digite seu telefone: &quot;))
+let pergunta = prompt(&quot;Suas informaçoes estao corretas? Verifique:
+&quot;)
+console.log (&quot;Pronto!seu cadastro foi realizado com sucesso, agora
+voce pode comprar os melhores produtos que nossa loja tem a
+oferecer&quot;)
+
+
+let data = prompt(&quot;Qual o ano do seu nascimento: &quot;)
+let ano = 2025
+idade = ano - data
+console.log(&quot;A idade inserida de&quot;, idade, &quot;anos,nao
+corresponde,voce deve ser maior de idade, coloque uma idade
+valida&quot;)
