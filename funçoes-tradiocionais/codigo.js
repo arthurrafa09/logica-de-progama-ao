92850cd97e4079df28ca34179b51cc79b2c7489f
Arthur <mioliara2020@gmail.com>
2025-06-03 10:22:33 -0300

    codigo.js

---
+++ funçoes-tradiocionais/codigo.js
@@ -0,0 +1,118 @@
+//1
+function saudaçao(){
+console.log(&quot;Olá, Mundo!&quot;)
+}
+saudaçao()
+//2
+function mostrarNome(){
+console.log(&quot;Arthur Rafael&quot;)
+}
+mostrarNome()
+//3
+function mostrarIdade(){
+console.log(&quot;16&quot;)
+}
+mostrarIdade()
+//4
+let num1 = 10
+let num2 = 20
+function somarDoisNumeros(){
+resultado = num1 + num2
+return resultado
+}
+console.log (somarDoisNumeros())
+//5
+function mostrarMensagem(){
+console.log(&quot;Para o sucesso é preciso suporta o processo&quot;)
+}
+mostrarMensagem()
+//6
+function mostrarData(){
+console.log(&quot;06/05/25&quot;)
+}
+mostrarData()
+//7
+function mostrarCidade(){
+console.log(&quot;itaquera&quot;)
+}
+mostrarCidade()
+//8
+function saudaçao(){
+console.log(&quot;Bem vindo Arthur&quot;)
+
+}
+saudaçao()
+//9
+let num1 = Number(prompt(&quot;Digite o numero 1: &quot;))
+let num2 = Number(prompt(&quot;Digite o numero 2: &quot;))
+function subtrairDoisNumeros(){
+resultado = num1 - num2
+return resultado
+}
+console.log (subtrairDoisNumeros())
+//10
+function mostrarAnimalFavorito(){
+console.log(&quot;Cachorro&quot;)
+}
+mostrarAnimalFavorito()
+//11
+function Aprendendo(){
+console.log(&quot;Estou Aprendendo JavaScript&quot;)
+}
+Aprendendo()
+// 12
+let num1 = Number(prompt(&quot;Digite o numero 1: &quot;))
+function triplicarNumero(){
+resultado = num1 * 3
+return resultado
+}
+console.log(&quot;O triplo desse numero é:&quot;,triplicarNumero())
+// 13
+function nomeIdade(){
+let nome = prompt(&quot;Digite seu nome: &quot;)
+let idade = Number(prompt(&quot;Digite sua idade: &quot;))
+console.log (&quot;Seu nome é&quot;,nome,&quot;e sua idade é&quot;, idade)
+}
+nomeIdade()
+
+//14
+function contagem1a10(){
+console.log(&quot;1 2 3 4 5 6 7 8 9 10&quot;)
+}
+
+contagem1a10()
+//15
+function inicioDeAula(){
+console.log(&quot;Aula de funçoes iniciada&quot;)
+}
+inicioDeAula()
+//16
+function seuNumero(){
+let numero =Number(prompt(&quot;Digite um numero: &quot;))
+console.log(&quot;Voce digitou o numero&quot;,numero)
+}
+seuNumero()
+//17
+function curso(){
+console.log(&quot;Desing grafico&quot;)
+}
+curso()
+//18
+function chamada(){
+console.log(&quot;Arthur Rafael, compareça ao caixa 4 por favor&quot;)
+}
+chamada()
+
+//19
+function cor(){
+let cor=prompt(&quot;qual sua cor favorita? &quot;)
+console.log(&quot;sua cor favortita é&quot;, cor)
+}
+cor()
+
+//20
+function país(){
+let país=prompt(&quot;qual país voce mora? &quot;)
+console.log(&quot;voce mora no&quot;, país)
+}
+país()
