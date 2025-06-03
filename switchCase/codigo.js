c87fa46dd1feb5215632d170cc30bfdeaa0d1ff5
Arthur <mioliara2020@gmail.com>
2025-06-03 09:37:21 -0300

    codigo.js

---
+++ switchCase/codigo.js
@@ -0,0 +1,111 @@
+let RespostaCorreta = 0;
+let RespostaIncorreta = 0;
+
+// Pergunta 1
+console.log("Que dia é o aniversario do neymar?");
+console.log("A - 25/03");
+console.log("B - 27/05");
+console.log("C - 05/02");
+console.log("D - 30/01");
+let pergunta1 = prompt("Digite a alternativa correta: ");
+switch (pergunta1) {
+    case "C":
+    case "05/02":
+        console.log("Alternativa correta!");
+        RespostaCorreta++;
+        break;
+    default:
+        console.log("Alternativa errada!");
+        RespostaIncorreta++;
+}
+
+// Pergunta 2
+console.log("Qual idade do neymar?");
+console.log("A - 25");
+console.log("B - 27");
+console.log("C - 34");
+console.log("D - 33");
+let pergunta2 = prompt("Digite a alternativa correta: ");
+switch (pergunta2) {
+    case "D":
+    case "33":
+        console.log("Alternativa correta!");
+        RespostaCorreta++;
+        break;
+    default:
+        console.log("Alternativa errada!");
+        RespostaIncorreta++;
+}
+
+// Pergunta 3
+console.log("Quantas bolas de ouro o neymar tem?");
+console.log("A - 5");
+console.log("B - 0");
+console.log("C - 3");
+console.log("D - 1");
+let pergunta3 = prompt("Digite a alternativa correta: ");
+switch (pergunta3) {
+    case "B":
+    case "0":
+        console.log("Alternativa correta!");
+        RespostaCorreta++;
+        break;
+    default:
+        console.log("Alternativa errada!");
+        RespostaIncorreta++;
+}
+
+// Pergunta 4
+console.log("Quantos filhos neymar tem?");
+console.log("A - 4");
+console.log("B - 3");
+console.log("C - 5");
+console.log("D - 17");
+let pergunta4 = prompt("Digite a alternativa correta: ");
+switch (pergunta4) {
+    case "A":
+    case "4":
+        console.log("Alternativa correta!");
+        RespostaCorreta++;
+        break;
+    default:
+        console.log("Alternativa errada!");
+        RespostaIncorreta++;
+}
+
+// Pergunta 5
+console.log("Quem é o filho(a) mais velho que neymar tem?");
+console.log("A - davi lucca");
+console.log("B - mavie");
+console.log("C - helena");
+console.log("D - mel");
+let pergunta5 = prompt("Digite a alternativa correta: ");
+switch (pergunta5) {
+    case "A":
+    case "davi lucca":
+        console.log("Alternativa correta!");
+        RespostaCorreta++;
+        break;
+    default:
+        console.log("Alternativa errada!");
+        RespostaIncorreta++;
+}
+
+// Pergunta 6
+console.log("Qual time neymar joga atualmente?");
+console.log("A - PSG");
+console.log("B - Barcelona");
+console.log("C - Pindamonhangaba");
+console.log("D - Santos");
+let pergunta6 = prompt("Digite a alternativa correta: ");
+switch (pergunta6) {
+    case "D":
+    case "Santos":
+        console.log("Alternativa correta!");
+        RespostaCorreta++;
+        break;
+    default:
+        console.log("Alternativa errada!");
+        RespostaIncorreta++;
+}
+
