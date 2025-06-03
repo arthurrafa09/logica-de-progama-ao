5a12f779d539864f8a48b1342b2ddbe89b452b84
Arthur <mioliara2020@gmail.com>
2025-06-03 09:00:55 -0300

    codigo.js

---
+++ variaveis/codigo.js
@@ -0,0 +1,26 @@
+console.log(nome)
+let idade = "15"
+console.log(idade)
+let aprendendoJavaScript = true
+console.log(aprendendoJavaScript)
+let nome = "Arthur"
+let sobrenome = "Rafael"
+let nomeCompleto = (nome + sobrenome)
+console.log(nomeCompleto)
+const num1 = 15
+const num2 = 37
+console.log(num1 + num2)
+let num1 = 15
+let num2 = 37
+console.log(num1 * num2)*/
+/*const cidade = "ferraz"*/
+const habitantes = 3000
+console.log("Em", cidade, "tem", habitantes, "habitantes")
+let preco = 69
+preco += 69
+console.log(preco)
+let meuValor
+console.log(meuValor)
+let meuNulo = "nu11"
+console.log(meuNulo)
+
