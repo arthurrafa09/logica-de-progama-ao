24e1f42839cd354795c3ebc5d1b7468031aeffe4
Arthur <mioliara2020@gmail.com>
2025-06-03 09:17:47 -0300

    codigo.js

---
+++ perguntas-programiz/codigo.js
@@ -0,0 +1,154 @@
+let RespostaCorreta = 0
+let RespostaIncorreta = 0
+
+// Pergunta 1
+console.log("Em qual ano estamos?")
+console.log("A - 2023")
+console.log("B - 2005")
+console.log("C - 2025")
+console.log("D - 2010")
+let pergunta1 = prompt("Digite a alternativa correta: ")
+if (pergunta1 == "C" || pergunta1 == "c") {
+    console.log("Alternativa Correta!")
+    RespostaCorreta++
+} else {
+    console.log("Alternativa Errada!")
+    RespostaIncorreta++
+}
+
+// Pergunta 2
+console.log("Qual dia se comemora a véspera de natal?")
+console.log("A - 25/12")
+console.log("B - 24/12")
+console.log("C - 31/12")
+console.log("D - 21/12")
+let pergunta2 = prompt("Digite a alternativa correta: ")
+if (pergunta2 == "B" || pergunta2 == "b") {
+    console.log("Alternativa Correta!")
+    RespostaCorreta++
+} else {
+    console.log("Alternativa Errada!")
+    RespostaIncorreta++
+}
+
+// Pergunta 3
+console.log("Quantas Ballon d'or (Bola de Ouro) o Neymar possui?")
+console.log("A - 6")
+console.log("B - 4")
+console.log("C - 2")
+console.log("D - 0")
+let pergunta3 = prompt("Digite a alternativa correta: ")
+if (pergunta3 == "D" || pergunta3 == "d") {
+    console.log("Alternativa Correta!")
+    RespostaCorreta++
+} else {
+    console.log("Alternativa Errada!")
+    RespostaIncorreta++
+}
+
+// Pergunta 4
+console.log("Qual foi o resultado que o Brasil perdeu para a Alemanha?")
+console.log("A - 6x1")
+console.log("B - 9x1")
+console.log("C - 7x1")
+console.log("D - 10x0")
+let pergunta4 = prompt("Digite a alternativa correta: ")
+if (pergunta4 == "C" || pergunta4 == "c") {
+    console.log("Alternativa Correta!")
+    RespostaCorreta++
+} else {
+    console.log("Alternativa Errada!")
+    RespostaIncorreta++
+}
+
+// Pergunta 5
+console.log("Em qual ano foi que o Brasil ganhou a Copa do Mundo?")
+console.log("A - 2022")
+console.log("B - 2002")
+console.log("C - 1994")
+console.log("D - 1990")
+let pergunta5 = prompt("Digite a alternativa correta: ")
+if (pergunta5 == "B" || pergunta5 == "b") {
+    console.log("Alternativa Correta!")
+    RespostaCorreta++
+} else {
+    console.log("Alternativa Errada!")
+    RespostaIncorreta++
+}
+
+// Pergunta 6
+console.log("Qual foi o último celular lançado pela Apple?")
+console.log("A - iPhone 15")
+console.log("B - iPhone 14")
+console.log("C - iPhone 16 Pro Max")
+console.log("D - iPhone 16")
+let pergunta6 = prompt("Digite a alternativa correta: ")
+if (pergunta6 == "C" || pergunta6 == "c") {
+    console.log("Alternativa Correta!")
+    RespostaCorreta++
+} else {
+    console.log("Alternativa Errada!")
+    RespostaIncorreta++
+}
+
+// Pergunta 7
+console.log("Em qual ano foi que o Brasil ganhou a Copa do Pentacampeão?")
+console.log("A - 1958")
+console.log("B - 1962")
+console.log("C - 1964")
+console.log("D - 1994")
+let pergunta7 = prompt("Digite a alternativa correta: ")
+if (pergunta7 == "B" || pergunta7 == "b") {
+    console.log("Alternativa Correta!")
+    RespostaCorreta++
+} else {
+    console.log("Alternativa Errada!")
+    RespostaIncorreta++
+}
+
+// Pergunta 8
+console.log("Qual foi o último celular lançado pela Apple?")
+console.log("A - iPhone 15")
+console.log("B - iPhone 14")
+console.log("C - iPhone 16 Pro Max")
+console.log("D - iPhone 16")
+let pergunta8 = prompt("Digite a alternativa correta: ")
+if (pergunta8 == "C" || pergunta8 == "c") {
+    console.log("Alternativa Correta!")
+    RespostaCorreta++
+} else {
+    console.log("Alternativa Errada!")
+    RespostaIncorreta++
+}
+
+// Pergunta 9
+console.log("Em que dia as aulas voltaram?")
+console.log("A - 05/02")
+console.log("B - 01/02")
+console.log("C - 10/02")
+console.log("D - 27/01")
+let pergunta9 = prompt("Digite a alternativa correta: ")
+if (pergunta9 == "B" || pergunta9 == "b") {
+    console.log("Alternativa Correta!")
+    RespostaCorreta++
+} else {
+    console.log("Alternativa Errada!")
+    RespostaIncorreta++
+}
+
+// Pergunta 10
+console.log("Qual o nome dos nossos professores(as) do SENAI?")
+console.log("A - Marcos e Marcello")
+console.log("B - Marcelo e Matheus")
+console.log("C - Luciana e Elaine")
+console.log("D - Marcos e Matheus")
+let pergunta10 = prompt("Digite a alternativa correta: ")
+if (pergunta10 == "D" || pergunta10.toLowerCase() == "marcos e matheus") {
+    console.log("Alternativa Correta!")
+    RespostaCorreta++
+} else {
+    console.log("Alternativa Errada!")
+    RespostaIncorreta++
+}
+
+
